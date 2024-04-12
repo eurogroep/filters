bd27bab12ecc03e5dc1b8bdda6c1de0950773448
Jeanine van Bruggen <jeanine.vanbruggen@lowpad.com>
2024-04-12 10:49:24 +0200

    solve dynamic typing runtime error

--- include/filters/filter_chain.hpp
+++ include/filters/filter_chain.hpp
@@ -78,11 +78,17 @@ load_chain_config(
     name_desc.name = norm_param_prefix + filter_n + ".name";
     name_desc.type = rcl_interfaces::msg::ParameterType::PARAMETER_STRING;
     name_desc.read_only = false;
+    // Must be dynamically typed because later we undeclare it and redeclare
+    // it read-only, but statically typed params cannot be undeclared.
+    name_desc.dynamic_typing = true;
 
     rcl_interfaces::msg::ParameterDescriptor type_desc;
     type_desc.name = norm_param_prefix + filter_n + ".type";
     type_desc.type = rcl_interfaces::msg::ParameterType::PARAMETER_STRING;
     type_desc.read_only = false;
+    // Must be dynamically typed because later we undeclare it and redeclare
+    // it read-only, but statically typed params cannot be undeclared.
+    type_desc.dynamic_typing = true;
 
     node->declare_parameter(
       name_desc.name, rclcpp::ParameterValue(), name_desc);
